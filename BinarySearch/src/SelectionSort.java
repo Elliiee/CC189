dded12aebc6b6604b35e91ab479a9ffea2b338f3
Elliiee <danyiluozhang@gmail.com>
2019-11-11 20:35:39 -0600

    Selection Sort

---
+++ BinarySearch/src/SelectionSort.java
@@ -0,0 +1,15 @@
+public class SelectionSort {
+    void sort(int[] nums){
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++){
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++){
+                if (nums[j] < nums[min_idx])
+                    min_idx = j;
+            }
+            int temp = nums[min_idx];
+            nums[min_idx] = nums[i];
+            nums[i] = temp;
+        }
+    }
+}
