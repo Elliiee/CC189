a48c68bdcab3d9da8c11501929daa75e3e66b364
Elliiee <danyiluozhang@gmail.com>
2020-03-23 21:33:54 -0500

    cc 4.1

--- TreesAndGraphs/src/_4_1.java
+++ TreesAndGraphs/src/_4_1.java
@@ -15,7 +15,7 @@ public class _4_1 {
     // BFS
     enum State {Unvisited, Visited, Visiting; }
 
-    boolean search(Graph g, Node start, Node end){
+    public static boolean search(Graph g, Node start, Node end){
         if (start == end) return true;
 
         LinkedList<Node> q = new LinkedList<>();
@@ -44,5 +44,37 @@ public class _4_1 {
             }
         }
         return false;
+    }
+
+    public static Graph createNewGraph()
+    {
+        Graph g = new Graph();
+        Node[] temp = new Node[6];
+
+        temp[0] = new Node("a", 3);
+        temp[1] = new Node("b", 0);
+        temp[2] = new Node("c", 0);
+        temp[3] = new Node("d", 1);
+        temp[4] = new Node("e", 1);
+        temp[5] = new Node("f", 0);
+
+        temp[0].addAdjacent(temp[1]);
+        temp[0].addAdjacent(temp[2]);
+        temp[0].addAdjacent(temp[3]);
+        temp[3].addAdjacent(temp[4]);
+        temp[4].addAdjacent(temp[5]);
+        for (int i = 0; i < 6; i++) {
+            g.addNode(temp[i]);
+        }
+        return g;
+    }
+
+    public static void main(String a[])
+    {
+        Graph g = createNewGraph();
+        Node[] n = g.getNodes();
+        Node start = n[3];
+        Node end = n[5];
+        System.out.println(search(g, start, end));
     }
 }
